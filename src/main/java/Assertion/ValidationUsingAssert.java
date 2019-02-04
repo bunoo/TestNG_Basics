f694a4c5c8542c100128cbb0fa9cfda9f486b914
abha <abha@example.com>
2019-02-04 14:00:11 +0530

    user#2 makes first commit

--- src/main/java/Assertion/ValidationUsingAssert.java
+++ src/main/java/Assertion/ValidationUsingAssert.java
@@ -1,13 +1,14 @@
 package Assertion;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+//user#2 makes first commit
 
 public class ValidationUsingAssert {
 
